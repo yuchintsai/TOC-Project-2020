65242abadb24ca0c9a121ce6d1dedd4b3840b10e
yuchin <st9540623@gmail.com>
2019-12-06 17:19:48 +0800

    a

--- fsm.py
+++ fsm.py
@@ -19,7 +19,7 @@ class TocMachine(GraphMachine):
         print("I'm entering welcome")
 
         reply_token = event.reply_token
-        print(event.source.userId)
+        print(event.source)
         send_text_message(reply_token, "歡迎使用")
         send_text_message(reply_token, "歡迎使用2")
 
