b2a4c53c137aa69e0c8b5adef76a48b47a3d4eba
yuchin <st9540623@gmail.com>
2019-12-08 22:50:15 +0800

    txt

--- fsm.py
+++ fsm.py
@@ -40,34 +40,34 @@ class TocMachine(GraphMachine):
         return text == "亞洲"
     
     def on_enter_welcome(self, event):
-        send_text_message(event, "歡迎使用")
+        send_text_message(event, "歡迎使用YCBot\n今天你想看什麼電影呢；)\n每天為你挑選適合你的影片\n輸入「地區」或「類型」\n用你最喜歡的方式搜尋！")
 
     def on_enter_type(self, event):
-        send_text_message(event, "可輸入\n「愛情」、「科幻」、「劇情」、「動畫」")
+        send_text_message(event, "輸入\n「愛情」、「科幻」、「劇情」、「動畫」\n尋找你想看的電影類型")
     def on_enter_region(self, event):
-        send_text_message(event, "可輸入\n「美洲」、「歐洲」、「台灣」、「亞洲」")
+        send_text_message(event, "可輸入\n「美洲」、「歐洲」、「台灣」、「亞洲」\n尋找你想看的區域的電影")
 
     def on_enter_love(self, event):
-        send_text_message(event, "《誰先愛上他的》")
+        send_text_message(event, " 2018《誰先愛上他的》\n「當有個人跟你說，他想當個正常人然後離開你，從那之後的每一天，就是一萬年」")
         self.go_back(event)
     def on_enter_animate(self, event):
-        send_text_message(event, "《COCO》")
+        send_text_message(event, "2017《COCO》\n「The real death is that no one in the world remembers you.」")
         self.go_back(event)
     def on_enter_scifi(self, event):
-        send_text_message(event, "《彗星來的那一夜》")
+        send_text_message(event, " 2014 《彗星來的那一夜》\n「科技來自於人性，那麼科幻就來自遺憾」")
         self.go_back(event)
     def on_enter_drama(self, event):
-        send_text_message(event, "《最美的禮物》")
+        send_text_message(event, "2016《最美的安排》\n「Just be sure to notice the collateral beauty. It's the profound connection to everthing.」")
         self.go_back(event)
     def on_enter_am(self, event):
-        send_text_message(event, "《猛毒》")
+        send_text_message(event, "2018《猛毒》\n「有時候別人會阻止我們，但到最後改變世界的是我們」")
         self.go_back(event)
     def on_enter_eu(self, event):
-        send_text_message(event, "《放牛班的春天》")
+        send_text_message(event, "2015《丹麥女孩》\n「You helped bring Lili to life but she's always been there.  」")
         self.go_back(event)
     def on_enter_tai(self, event):
-        send_text_message(event, "《血觀音》")
+        send_text_message(event, "2017《血觀音》\n「 世界上最可怕的不是眼前的刑罰，而是無愛的未來」")
         self.go_back(event)
     def on_enter_as(self, event):
-        send_text_message(event, "《與神同行》")
+        send_text_message(event, "2017《與神同行》\n「不要為過去的事，浪費新的眼淚」")
         self.go_back(event)
